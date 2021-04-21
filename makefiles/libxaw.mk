9ee8c0f79ea4376121c237b4f898748011484d95
Nathan <dugarnathan@gmail.com>
2021-04-21 11:29:55 -0400

    wtf

--- makefiles/libxaw.mk
+++ makefiles/libxaw.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS    += libxaw
 LIBXAW_VERSION := 1.0.13
-DEB_LIBXAW_V  ?= $(LIBXAW_VERSION)
+DEB_lLIBXAW_V   ?= $(LIBXAW_VERSION)
 
 libxaw-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://www.x.org/archive/individual/lib/libXaw-$(LIBXAW_VERSION).tar.gz{,.sig}
@@ -17,8 +17,7 @@ libxaw:
 else
 libxaw: libxaw-setup libx11 libxau libxmu xorgproto libxpm libxt libxext
 	cd $(BUILD_WORK)/libxaw && ./configure -C \
-		$(DEFAULT_CONFIGURE_FLAGS) \
-		--disable-xaw6
+		$(DEFAULT_CONFIGURE_FLAGS)
 	+$(MAKE) -C $(BUILD_WORK)/libxaw
 	+$(MAKE) -C $(BUILD_WORK)/libxaw install \
 		DESTDIR=$(BUILD_STAGE)/libxaw
@@ -27,24 +26,26 @@ endif
 
 libxaw-package: libxaw-stage
 # libxau.mk Package Structure
-	rm -rf $(BUILD_DIST)/libxaw7 $(BUILD_DIST)/libxaw7-dev
-	mkdir -p $(BUILD_DIST)/libxaw7{,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	rm -rf $(BUILD_DIST)/libxaw{7,-dev}
+	mkdir -p $(BUILD_DIST)/libxaw7/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	mkdir -p $(BUILD_DIST)/libxaw-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{include,lib,share}
 
 	# libxaw.mk Prep libxaw7
-	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libXaw{7,}.7.dylib $(BUILD_DIST)/libxaw7/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libXaw{.6,.7,6,7,7.7,6.6}.dylib $(BUILD_DIST)/libxaw7/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# libxaw.mk Prep libxaw7-dev
-	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libXaw7.{a,dylib} $(BUILD_DIST)/libxaw7-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{include,share} $(BUILD_DIST)/libxaw7-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	# libxaw.mk Prep libxaw-dev
+	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{libXaw{7.a,6.a,.dylib},pkgconfig} $(BUILD_DIST)/libxaw-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxaw-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cp -a $(BUILD_STAGE)/libxaw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share $(BUILD_DIST)/libxaw-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
 	# libxaw.mk Sign
 	$(call SIGN,libxaw7,general.xml)
 
 	# libxaw.mk Make .debs
 	$(call PACK,libxaw7,DEB_LIBXAW_V)
-	$(call PACK,libxaw7-dev,DEB_LIBXAW_V)
+	$(call PACK,libxaw-dev,DEB_LIBXAW_V)
 
 	# libxaw.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libxaw7 $(BUILD_DIST)/libxaw7-dev
-
+	rm -rf $(BUILD_DIST)/libxaw{7,-dev}
 .PHONY: libxaw libxaw-package
+
