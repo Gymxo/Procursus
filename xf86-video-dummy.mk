eaa3ba489eb55a8090c98e7f7e0cabb93c0df58f
Nathan <dugarnathan@gmail.com>
2021-05-20 17:29:23 -0400

    xf86-video-dummy

--- xf86-video-dummy.mk
+++ xf86-video-dummy.mk
@@ -2,7 +2,7 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS    += xf86-video-dummy
+SUBPROJECTS   += xf86-video-dummy
 DUMMY_VERSION := 0.3.8
 DEB_DUMMY_V   ?= $(DUMMY_VERSION)
 
@@ -14,34 +14,30 @@ ifneq ($(wildcard $(BUILD_WORK)/xf86-video-dummy/.build_complete),)
 xf86-video-dummy:
 	@echo "Using previously built xf86-video-dummy."
 else
-xf86-video-dummy: xf86-video-dummy-setup libx11 libxau libxmu xorgproto xxhash
-	cd $(BUILD_WORK)/xf86-video-dummy && ./configure -C \
-		--host=$(GNU_HOST_TRIPLE) \
-		--prefix=/usr \
-		--sysconfdir=$(MEMO_PREFIX)/etc \
-		--localstatedir=$(MEMO_PREFIX)/var
+xf86-video-dummy: xf86-video-dummy-setup xorg-server libpixman
+	cd $(BUILD_WORK)/xf86-video-dummy && ./autogen.sh -C \
+	$(DEFAULT_CONFIGURE_FLAGS) \
+	CFLAGS="$(CFLAGS) -I$(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/{pixman-1,xorg}"
 	+$(MAKE) -C $(BUILD_WORK)/xf86-video-dummy
 	+$(MAKE) -C $(BUILD_WORK)/xf86-video-dummy install \
-		DESTDIR=$(BUILD_STAGE)/xf86-video-dummy
-	+$(MAKE) -C $(BUILD_WORK)/xf86-video-dummy install \
-		DESTDIR=$(BUILD_BASE)
+		DESTDIR=$(BUILD_STAGE)/xserver-xorg-video-dummy
 	touch $(BUILD_WORK)/xf86-video-dummy/.build_complete
 endif
 
 xf86-video-dummy-package: xf86-video-dummy-stage
 # xf86-video-dummy.mk Package Structure
-	rm -rf $(BUILD_DIST)/xf86-video-dummy
+	rm -rf $(BUILD_DIST)/xserver-xorg-video-dummy
 	
 # xf86-video-dummy.mk Prep xf86-video-dummy
-	cp -a $(BUILD_STAGE)/xf86-video-dummy $(BUILD_DIST)
-	
+	cp -a $(BUILD_STAGE)/xserver-xorg-video-dummy $(BUILD_DIST)
+
 # xf86-video-dummy.mk Sign
-	$(call SIGN,xf86-video-dummy,general.xml)
+	$(call SIGN,xserver-xorg-video-dummy,general.xml)
 	
 # xf86-video-dummy.mk Make .debs
-	$(call PACK,xf86-video-dummy,DEB_DUMMY_V)
+	$(call PACK,xserver-xorg-video-dummy,DEB_DUMMY_V)
 	
 # xf86-video-dummy.mk Build cleanup
-	rm -rf $(BUILD_DIST)/xf86-video-dummy
+	rm -rf $(BUILD_DIST)/xserver-xorg-video-dummy
 
 .PHONY: xf86-video-dummy xf86-video-dummy-package
