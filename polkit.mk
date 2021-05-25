b6b3b818548e8405d785657715536a0ba4fa0a91
Nathan <dugarnathan@gmail.com>
2021-05-24 22:10:35 -0400

    polkit

--- polkit.mk
+++ polkit.mk
@@ -22,8 +22,7 @@ polkit: polkit-setup libx11 libxau libxmu xorgproto xxhash
 		--enable-libelogind=no \
 		--enable-introspection=no \
 		--enable-libsystemd-login=no \
-		--disable-test \
-		ac_cv_file__etc_gentoo_release=yes
+		--disable-test
 	find $(BUILD_WORK)/polkit -type f -exec sed -i 's/-Wl,--as-needed/-Wl/g' {} \;
 	+$(MAKE) -C $(BUILD_WORK)/polkit
 	+$(MAKE) -C $(BUILD_WORK)/polkit install \
