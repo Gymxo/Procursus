d55a396bf8cba631eacffa2ad6e23b897af68c24
Nathan <dugarnathan@gmail.com>
2021-10-05 16:31:22 -0400

    at-spi2-atk

--- makefiles/at-spi2-atk.mk
+++ makefiles/at-spi2-atk.mk
@@ -30,59 +30,49 @@ ifneq ($(wildcard $(BUILD_WORK)/at-spi2-atk/.build_complete),)
 at-spi2-atk:
 	@echo "Using previously built at-spi2-atk."
 else
-at-spi2-atk: at-spi2-atk-setup
+at-spi2-atk: at-spi2-atk-setup atk at-spi2-atk dbus glib2.0
 	cd $(BUILD_WORK)/at-spi2-atk/build && meson \
 	--cross-file cross.txt \
 	-Dintrospection=false \
 	..
 	ninja -C $(BUILD_WORK)/at-spi2-atk/build
 	+DESTDIR="$(BUILD_STAGE)/at-spi2-atk" ninja -C $(BUILD_WORK)/at-spi2-atk/build install
-	touch $(BUILD_WORK)/at-spi2-atk/.build_complete
+	$(call AFTER_BUILD,copy)
 endif
 
 at-spi2-atk-package: at-spi2-atk-stage
-	# at-spi2-core.mk Package Structure
-	rm -rf $(BUILD_DIST)/at-spi2-core $(BUILD_DIST)/libatspi2.0-{0,dev}
-	mkdir -p $(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX){$(MEMO_SUB_PREFIX)/{share/dbus-1/{accessibility-services,services},libexec},/Library/LaunchDaemons} \
-		$(BUILD_DIST)/libatspi2.0-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
-		$(BUILD_DIST)/libatspi2.0-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib/pkgconfig,include}
+	# at-spi2-atk.mk Package Structure
+	rm -rf $(BUILD_DIST)/libatk-adaptor $(BUILD_DIST)/libatk-bridge2.0-{0,dev}
+	mkdir -p $(BUILD_DIST)/libatk-adaptor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		$(BUILD_DIST)/libatk-bridge2.0-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		$(BUILD_DIST)/libatk-bridge2.0-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib/pkgconfig,include}
 
-	# at-spi2-core.mk at-spi2-core glib2.0 dbus gettext libx11 libxi libxtst
-	cp -a $(BUILD_MISC)/at-spi2-core/org.a11y.Bus.plist \
-		$(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX)/Library/LaunchDaemons/org.a11y.Bus.plist
-	cp -a $(BUILD_STAGE)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/!(dbus-1) \
-		$(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share
-	cp -a $(BUILD_STAGE)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec \
-		$(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
-	cp -a $(BUILD_STAGE)/at-spi2-core/$(MEMO_PREFIX)/etc \
-		$(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX)/
-	cp -a $(BUILD_MISC)/at-spi2-core/org.a11y.Bus.plist \
-		$(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/dbus-1/services
-	cp -a $(BUILD_MISC)/at-spi2-core/org.a11y.atspi.Registry.plist \
-		$(BUILD_DIST)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/dbus-1/accessibility-services
+	# at-spi2-atk.mk libatk-adaptor
+	cp -a $(BUILD_STAGE)/at-spi2-atk/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{gnome-settings-daemon-3.0,gtk-2.0} \
+		$(BUILD_DIST)/libatk-adaptor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
 
-	# at-spi2-core.mk Prep libatspi2.0-0
-	cp -a $(BUILD_STAGE)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libatspi.0.dylib \
-		$(BUILD_DIST)/libatspi2.0-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	# at-spi2-atk.mk Prep libatk-bridge2.0-0
+	cp -a $(BUILD_STAGE)/at-spi2-atk/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libatk-bridge-2.0.0.dylib \
+		$(BUILD_DIST)/libatk-bridge2.0-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# at-spi2-core.mk Prep libatspi2.0-dev
-	cp -a $(BUILD_STAGE)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libatspi.0.dylib|systemd) \
-		$(BUILD_DIST)/libatspi2.0-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/at-spi2-core/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/at-spi-2.0 \
-		$(BUILD_DIST)/libatspi2.0-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include
+	# at-spi2-atk.mk Prep libatk-bridge2.0-dev
+	cp -a $(BUILD_STAGE)/at-spi2-atk/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libatk-bridge-2.0.0.dylib|gnome-settings-daemon-3.0|gtk-2.0) \
+		$(BUILD_DIST)/libatk-bridge2.0-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/at-spi2-atk/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/at-spi2-atk \
+		$(BUILD_DIST)/libatk-bridge2.0-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include
 
 
-	# at-spi2-core.mk Sign
-	$(call SIGN,at-spi2-core,general.xml)
-	$(call SIGN,libatspi2.0-0,general.xml)
+	# at-spi2-atk.mk Sign
+	$(call SIGN,libatk-adaptor,general.xml)
+	$(call SIGN,libatk-bridge2.0-0,general.xml)
 
-	# at-spi2-core.mk Make .debs
-	$(call PACK,at-spi2-core,DEB_AT-SPI2-CORE_V)
-	$(call PACK,libatspi2.0-0,DEB_AT-SPI2-CORE_V)
-	$(call PACK,libatspi2.0-dev,DEB_AT-SPI2-CORE_V)
+	# at-spi2-atk.mk Make .debs
+	$(call PACK,libatk-adaptor,DEB_AT-SPI2-ATK_V)
+	$(call PACK,libatk-bridge2.0-0,DEB_AT-SPI2-ATK_V)
+	$(call PACK,libatk-bridge2.0-dev,DEB_AT-SPI2-ATK_V)
 
-	# at-spi2-core.mk Build cleanup
-	rm -rf $(BUILD_DIST)/at-spi2-core $(BUILD_DIST)/libatspi2.0-{0,dev}
+	# at-spi2-atk.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libatk-adaptor $(BUILD_DIST)/libatk-bridge2.0-{0,dev}
 
 .PHONY: at-spi2-atk at-spi2-atk-package
 
